b6f48bc8b2113beeee2f523cc4ceea6ef1915304
Daniel Maina <daniel.maina@frontdoorhome.com>
2024-08-28 17:47:09 +0300

    add convex mutation and query to create and read files

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,31 @@
+"use client";
+
+import {Button} from "@/components/ui/button";
+import {SignedIn, SignedOut, SignInButton, SignOutButton} from "@clerk/nextjs";
+import {useMutation, useQuery} from "convex/react";
+import {api} from "@/convex/_generated/api";
+
 export default function Home() {
+    const createFile = useMutation(api.files.createFile);
+    const files = useQuery(api.files.getFiles);
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        Content
+        <SignedIn>
+            <SignOutButton>
+                <Button>Sign Out</Button>
+            </SignOutButton>
+        </SignedIn>
+        {files?.map((file) => (<p key={file._id}>{file.name}</p>))}
+        <div className="bg-gray-300">
+            <Button className="bg-amber-400" onClick={() => createFile({name: "New File 2"})}>Add File</Button>
+        </div>
+        <SignedOut>
+            <SignInButton mode="modal">
+                <Button>
+                    Sign In
+                </Button>
+            </SignInButton>
+        </SignedOut>
     </main>
   );
 }
