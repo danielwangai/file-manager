3b69fdcb117d46bda6fc8648d8965c89541ae26f
Daniel Maina <daniel.maina@frontdoorhome.com>
2024-08-30 10:43:54 +0300

    must be authorized to create files:- - user must be part of an org to create files - or for personal accounts must be logged in to create files

--- convex/files.ts
+++ convex/files.ts
@@ -1,5 +1,12 @@
-import {mutation, query} from "./_generated/server";
+import {mutation, MutationCtx, query, QueryCtx} from "./_generated/server";
 import {ConvexError, v} from "convex/values";
+import {getUser} from "./users";
+
+export const hasAccessToOrg = async (ctx: QueryCtx | MutationCtx, tokenIdentifier: string, orgId: string) => {
+    const user = await getUser(ctx, tokenIdentifier);
+    return user.orgIds.includes(orgId)
+        || user.tokenIdentifier.includes(orgId); // for files created on personal accounts
+}
 
 export const createFile = mutation({
     args: {
@@ -8,8 +15,15 @@ export const createFile = mutation({
     },
     async handler(ctx, args) {
         const identity = await ctx.auth.getUserIdentity();
+
         if (!identity) {
             throw new ConvexError("must be logged in to manage files");
+        }
+
+        const isAuthorized = hasAccessToOrg(ctx, identity.tokenIdentifier, args.orgId)
+
+        if (!isAuthorized) {
+            throw new ConvexError("your are not authorized to access this organization.")
         }
 
         await ctx.db.insert("files", {
